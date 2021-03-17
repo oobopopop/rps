fc031103598ddb80ba9ae119d48f1bf3ba1fd9f9
Jean Levesque <oobopopop@protonmail.com>
2021-03-17 18:17:04 -0400

    Added variables to update the score, doesn't work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function capitalize(str) {
+function capitalise(str) {
     let firstLetter = str.slice(0, 1);
     let otherLetters = str.substring(1);
     return firstLetter.toUpperCase() + otherLetters.toLowerCase();
@@ -11,29 +11,44 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === 'Rock') && (computerSelection === 'Paper')) {
-    return 'You lose! Paper beats Rock.';
-    } else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
-    return 'You win! Rock beats Scissors.';
-    } else if ((playerSelection === 'Rock') && (computerSelection === 'Rock')) {
-    return 'It\'s a draw! Rock vs Rock.';
-    } else if ((playerSelection === 'Paper') && (computerSelection === 'Paper')) {
-    return 'It\'s a draw! Paper vs Paper.';
-    } else if ((playerSelection === 'Paper') && (computerSelection === 'Scissors')) {
-    return 'You lose! Scissors beats Paper.';
-    } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
-    return 'You win! Paper beats Rock.';
-    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
-    return 'You win! Scissors beats Paper.';
-    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Scissors')) {
-    return 'It\'s a draw! Scissors vs Scissors.';
-    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Rock')) {
-    return 'You lose! Rock beats Scissors.';
-    }
     
+    if ((playerSelection === 'Rock') && (computerSelection === 'Paper')) {
+        ++computerScore;
+        return 'You lose! Paper beats Rock.' + currentScore;
+    } else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
+        ++playerScore;
+        return 'You win! Rock beats Scissors.' + currentScore;
+    } else if ((playerSelection === 'Rock') && (computerSelection === 'Rock')) {
+        return 'It\'s a draw! Rock vs Rock.' + currentScore;
+    } else if ((playerSelection === 'Paper') && (computerSelection === 'Paper')) {
+        return 'It\'s a draw! Paper vs Paper.' + currentScore;
+    } else if ((playerSelection === 'Paper') && (computerSelection === 'Scissors')) {
+        ++computerScore;
+        return 'You lose! Scissors beats Paper.' + currentScore;
+    } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
+        ++playerScore;
+        return 'You win! Paper beats Rock.' + currentScore;
+    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
+        ++playerScore;
+        return 'You win! Scissors beats Paper.' + currentScore;  
+    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Scissors')) {
+        return 'It\'s a draw! Scissors vs Scissors.' + currentScore;
+    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Rock')) {
+        ++computerScore;
+        return 'You lose! Rock beats Scissors.' + currentScore;
+    }
 }
 
-const playerSelection = capitalize(prompt('Pick a move'));
+let playerScore = 0;
+let computerScore = 0;
+let currentScore = `
+
+Player: ${playerScore}
+Computer: ${computerScore}`;
+
+
+const playerSelection = capitalise(prompt('Pick a move'));
 const computerSelection = computerPlay();
+
 
 console.log(playRound(playerSelection, computerSelection));
