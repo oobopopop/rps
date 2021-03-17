6831af49929b3e1122b48d1d65e22e0d913e107c
Jean Levesque <oobopopop@protonmail.com>
2021-03-17 18:54:03 -0400

    Fixed my currentScore problem

--- script.js
+++ script.js
@@ -14,27 +14,63 @@ function playRound(playerSelection, computerSelection) {
     
     if ((playerSelection === 'Rock') && (computerSelection === 'Paper')) {
         ++computerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You lose! Paper beats Rock.' + currentScore;
     } else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
         ++playerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You win! Rock beats Scissors.' + currentScore;
     } else if ((playerSelection === 'Rock') && (computerSelection === 'Rock')) {
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'It\'s a draw! Rock vs Rock.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Paper')) {
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'It\'s a draw! Paper vs Paper.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Scissors')) {
         ++computerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You lose! Scissors beats Paper.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
         ++playerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You win! Paper beats Rock.' + currentScore;
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
         ++playerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You win! Scissors beats Paper.' + currentScore;  
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Scissors')) {
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'It\'s a draw! Scissors vs Scissors.' + currentScore;
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Rock')) {
         ++computerScore;
+        currentScore = `
+
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
         return 'You lose! Rock beats Scissors.' + currentScore;
     }
 }
