af43df3adb9ffcbbf64545b365cc07add5e98504
Jean Levesque <oobopopop@protonmail.com>
2021-03-18 00:08:39 -0400

    Cleanup + playRound can be called multiple times

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+let currentScore;
+let computerSelection;
+let playerSelection;
+
 function capitalise(str) {
     let firstLetter = str.slice(0, 1);
     let otherLetters = str.substring(1);
@@ -10,81 +16,70 @@ function computerPlay() {
     return randomPlay;
 }
 
-function playRound(playerSelection, computerSelection) {
+function updateScore() {
+    currentScore = `
+Player: ${playerScore}
+Computer: ${computerScore}`;
+}
+
+function playRound() {
     
+    computerSelection = computerPlay();
+    playerSelection = capitalise(prompt('Pick a move'));
+
     if ((playerSelection === 'Rock') && (computerSelection === 'Paper')) {
         ++computerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You lose! Paper beats Rock.' + currentScore;
     } else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
         ++playerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You win! Rock beats Scissors.' + currentScore;
     } else if ((playerSelection === 'Rock') && (computerSelection === 'Rock')) {
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'It\'s a draw! Rock vs Rock.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Paper')) {
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'It\'s a draw! Paper vs Paper.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Scissors')) {
         ++computerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You lose! Scissors beats Paper.' + currentScore;
     } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
         ++playerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You win! Paper beats Rock.' + currentScore;
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
         ++playerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You win! Scissors beats Paper.' + currentScore;  
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Scissors')) {
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'It\'s a draw! Scissors vs Scissors.' + currentScore;
     } else if ((playerSelection === 'Scissors') && (computerSelection === 'Rock')) {
         ++computerScore;
-        currentScore = `
-
-        Player: ${playerScore}
-        Computer: ${computerScore}`;
+        updateScore()
         return 'You lose! Rock beats Scissors.' + currentScore;
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let currentScore = `
 
-Player: ${playerScore}
-Computer: ${computerScore}`;
+function game() {
+    
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+
+    if (playerScore > computerScore) {
+        console.log('You are a big winner!');
+    } else if (playerScore < computerScore) {
+        console.log('You are a big loser!');
+    } else {
+        console.log('Draw.');
+    }  
+}
 
 
-const playerSelection = capitalise(prompt('Pick a move'));
-const computerSelection = computerPlay();
-
-
-console.log(playRound(playerSelection, computerSelection));
+game()
